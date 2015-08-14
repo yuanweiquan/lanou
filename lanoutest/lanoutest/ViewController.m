cc40af25bb66e7f6dd9dff23dc57ce6e75af73b8
www <66666666>
2015-08-14 10:24:17 +0800

    tianjial

--- lanoutest/lanoutest/ViewController.m
+++ lanoutest/lanoutest/ViewController.m
@@ -16,7 +16,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    NSLog(@"3333333333");
 }
 
 - (void)didReceiveMemoryWarning {
